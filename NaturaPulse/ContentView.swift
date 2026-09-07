c3d761e19e149c5de671d7fd98c313b41d54f489
rfbnr <ridwanfebnur9@gmail.com>
2026-09-07 21:22:23 +0700

    Initial Commit

---
+++ NaturaPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NaturaPulse
+//
+//  Created by Ridwan Febnur AR on 07/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NaturaPulse/NaturaPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaturaPulseApp.swift
+//  NaturaPulse
+//
+//  Created by Ridwan Febnur AR on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NaturaPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
